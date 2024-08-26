ab6e06e6fb1e49c8670a46cde8fd00e1d6d5fb7a
Samuel Nnanna <samuelnnanna71@gmail.com>
2024-08-26 23:14:44 +0100

    test

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd node-app
+terraform init
+terraform apply --auto-approve
+ELB_EP=$(terraform output --raw alb_dns_name)
+
+cd ../angular-app
+terraform init
+TF_VAR_alb_dns_name=$ELB_EP terraform apply --auto-approve
